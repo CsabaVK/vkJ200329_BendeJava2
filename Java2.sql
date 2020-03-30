816c0c616fb478fd6cba112151855e219133ca84
Csaba <56678429+CsabaVK@users.noreply.github.com>
2020-03-30 12:56:13 +0200

    Add files via upload

---
+++ Java2.sql
@@ -0,0 +1,9 @@
+﻿("Papp Judit","10001","Budapest","Keresztúri út 210.","1046"),
+("Pióker Balázs","10002","Budapest","Toldy F. u. 12.","1021"),
+("Maklári Ibolya","10003","Budapest","Borko u. 8.","1052"),
+("Péter Tímea","10004","Budapest","Kövér Lajos u. 56.","1095"),
+("Végvári József","10005","Budaörs","Mészáros u. 48-54.","1215"),
+("Vanek Anna","10006","Budapest","Rumbach S.u.6.","1113"),
+("Vass Pál","10007","Budapest","Báthory u. 12.","1051"),
+("Véber Zoltán","10008","Budapest","Hársfa u. 21.","1139"),
+("Varga Attila","10009","Maglód","Huszár u. 7.","1103");
